549308587a904b12f729da9f3272824a163ff40a
Jairo <jairosalguedo@gmail.com>
2023-07-14 06:44:34 -0500

    Agregar archivos de proyecto.

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,15 @@
+﻿   //EJERCICIO #1  
+ string nombre = "Jairo Salguedo";
+    Console.WriteLine($"Hola, {nombre}");
+
+   //EJERCICIO #2
+  DateTime horaActual = DateTime.Now;
+
+     string horaFormateada = horaActual.ToString("HH:mm:ss");
+
+        Console.WriteLine("La hora actual es: " + horaFormateada);
+        Console.ReadKey();
+    
+
+    
+
